56ca0ae2481bdfa4cb18ee7efb0944992577c828
Irina Filimonova <flllmonova@yandex.ru>
2026-04-25 03:55:11 +0500

    feat: solve a tsk "towel-sort"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  const ans = [];
+  if (arguments.length === 0) {
+    return ans;
+  }
+  matrix.forEach((item, index) => {
+    if (index % 2 === 0) {
+      ans.push(...item);
+    } else {
+      ans.push(...item.reverse());
+    }
+  });
+  return ans;
 };
